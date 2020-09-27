787094576c0efe64345ba50a77e4a58baf8bd4a4
Unknown <monique.cockram@students.mq.edu.au>
2020-09-27 10:02:08 +1000

    control code initial commit

---
+++ control_code/control_code.ino
@@ -0,0 +1,13 @@
+// NCal control code
+// PHYS6701 S2 2020
+// Monique Cockram and Ryan Husband
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
