e212f2fba2aa061dcc2811b1d5109ec0ae0fcd55
Unknown <monique.cockram@students.mq.edu.au>
2020-09-27 11:10:32 +1000

    pseudocode added

--- control_code/control_code.ino
+++ control_code/control_code.ino
@@ -2,12 +2,38 @@
 // PHYS6701 S2 2020
 // Monique Cockram and Ryan Husband
 
+
+//int pins for the motor
+int motor = 1 //just a placeholder pin for now
+// need to do a speed pin too???
+
+
+
 void setup() {
   // put your setup code here, to run once:
+
+//initialise pins
+  pinMode(motor, OUTPUT)
+
+
+//do a slow speed up of the wheel
+//write(motor, )
+//delay(1000)
+//write(motor, slightly faster)
+//delay(1000)
+//etc
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+
+// set wheel speed to ___
+// read light gate output data
+// calculate rotation speed speed = (2*pi*r/50)/time
+// adjust rotation speed accordingly (if loop?)
+
 
 }
