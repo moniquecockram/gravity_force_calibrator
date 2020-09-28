5e250d8542aba1c39f25754461213595c1addca5
Unknown <monique.cockram@students.mq.edu.au>
2020-09-28 11:04:07 +1000

    pseudocode expanded on

--- control_code/control_code.ino
+++ control_code/control_code.ino
@@ -7,8 +7,6 @@
 int motor = 1 //just a placeholder pin for now
 // need to do a speed pin too???
 
-
-
 void setup() {
   // put your setup code here, to run once:
 
@@ -30,10 +28,15 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 
-// set wheel speed to ___
-// read light gate output data
-// calculate rotation speed speed = (2*pi*r/50)/time
+// set wheel speed to chosen value
+// read light gate output data (should be a time between last peak?
+// calculate rotation speed speed = (C/number of holes)/time = (2*pi*r/50)/time
 // adjust rotation speed accordingly (if loop?)
+      // if speed < chosen value
+            // set_speed += some small value
+      // if speed > chosen value
+            // set_speed -= some small value
+     // else do nothing
 
 
 }
